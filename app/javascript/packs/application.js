02d87b9169a60fafd43448d9c4c6bd009e193945
Ryrya Astrey <119737958+RyryaAstrey@users.noreply.github.com>
2023-01-09 03:33:13 +0000

    4-24レイアウト途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "jquery"
-import "pcpper.js";
+import "jquery";
+import "popper.js";
 import "bootstrap";
-import "../stylesheet/application"
-import "@fortawesome/fontawesome-free/js/all"
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
